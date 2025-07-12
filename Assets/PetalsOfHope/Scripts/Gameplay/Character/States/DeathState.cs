9b63f0bf565a89eebdc9ba09a052d365f40c8b8a
hoshyar Mahmoudi <hoshicameron@gmail.com>
2025-07-13 00:18:16 +0330

    feat add respawn system

--- Assets/PetalsOfHope/Scripts/Gameplay/Character/States/DeathState.cs
+++ Assets/PetalsOfHope/Scripts/Gameplay/Character/States/DeathState.cs
@@ -27,18 +27,8 @@ namespace PetalsOfHope.Gameplay.States
             _characterController.InputSource.DisableAllInput();
             
             // Stop all movement
-            _characterController.Rigidbody.linearVelocity = Vector2.zero;
+            _characterController.Rigidbody.velocity = Vector2.zero;
             _characterController.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
-            
-            // Disable collider after a short delay to allow death animation to play
-            var collider = _characterController.GetComponent<Collider2D>();
-            if (collider != null)
-            {
-                collider.enabled = false;
-            }
-            
-            // Disable other components that might interfere
-            _characterController.enabled = false;
         }
 
         public override void Exit()
